5ebb5ed8ab9a8c875497518744649f97807498d1
Dang-Thanh Tran <trandgthanh@gmail.com>
2018-08-02 11:28:19 +0200

    Added jquery masonry and styles

--- Gemfile
+++ Gemfile
@@ -20,6 +20,8 @@ gem 'bootstrap-sass'
 gem 'simple_form'
 gem 'devise'
 gem 'paperclip'
+gem 'masonry-rails'
+gem 'jquery-rails'
 
 group :development, :test do
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,7 +10,9 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require rails-ujs
+//= require jquery
+//= require jquery_ujs
+//= require masonry/jquery.masonry
 //= require bootstrap-sprockets
 //= require activestorage
 //= require turbolinks
